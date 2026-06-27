eaeb9492e86d627fb25755b5f5e05e6282e4daa6
YSY114243 <17yahyay@gmail.com>
2026-06-27 14:12:40 +0300

    fix: remove invalid task_type import in dashboard_screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -3,7 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/daily_log.dart';
-import '../models/task_type.dart';
 import '../services/supabase_service.dart';
 import '../widgets/app_sidebar.dart';
 import '../widgets/intern_log_logo.dart';
